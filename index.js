e4999836a9d087c3b34610f946a18245c9424ce8
Shadota <48773700+Shadota@users.noreply.github.com>
2025-12-24 23:14:30 +0100

    Avoid double-trimming already removed messages

--- index.js
+++ index.js
@@ -3637,6 +3637,9 @@ function get_injection_threshold() {
                     }
                     for (let i = next_index; i < step_end; i++) {
                         let message = chat[i]
+                        if (get_data(message, 'lagging') === false) {
+                            continue
+                        }
                         let message_tokens = count_tokens(message.mes)
                         let summary = get_memory(message)
                         let summary_tokens = summary && check_message_exclusion(message)
